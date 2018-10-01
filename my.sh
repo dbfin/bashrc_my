1f0c9ab59261ba2b151ce1076851f1b0bd81abee
vadim <vadim@dbfin.com>
2018-10-01 11:24:33 -0500

    Simple welcome message.

--- my.sh
+++ my.sh
@@ -32,37 +32,7 @@ unset ssh
 	[[ ${#HOST} -gt 4 ]] && ssh=$ssh$HOST[${#HOST}] 
 }
 
-local fortune=$(command -v fortune 2>/dev/null) || fortune=''
-[ -n "$fortune" ] && {
-
-local W=69
-local WT=54
-local WB="\e[44m"
-local WF="\e[97m"
-local WFF="\e[93m"
-local pad="$( printf "%${W}s" " " )"
-local pad_="$( printf "%${W}s" " " | sed 's| |─|g' )"
-local msg=" Welcome, $user! "
-
-local text="$( $fortune -n 256 -s 2>/dev/null )"
-export QUOTE="$( echo "$text" | tr '\n' ' ' | sed 's|\s\+| |g' )"
-echo -en "\e[0m\n${WB}${WF}"
-printf '%s%s%s%s%s' '╭' "${pad_:1:$(( ($W-2-${#msg})/2 ))}" "$msg" "${pad_:1:$(( ($W-2-${#msg}+1)/2 ))}" '╮'
-echo -en "\e[0m\n${WB}${WF}"
-printf '%s%s%s' '│' "${pad:1:$(( $W-2 ))}" '│'
-while read l; do
-	echo -en "\e[0m\n${WB}${WF}│ ${WFF}"
-	printf '%s%s' "${pad:1:$(( $W-4-${#l} ))}" "$( expand -t 1 <<< "$l" )"
-	echo -en "${WF} │"
-done < <( echo "$text" | fold -s -w $WT )
-echo -en "\e[0m\n${WB}${WF}"
-printf '%s%s%s' '│' "${pad:1:$(( $W-2 ))}" '│'
-echo -en "\e[0m\n${WB}${WF}"
-printf '%s%s%s' '╰' "${pad_:1:$(( $W-2 ))}" '╯'
-echo -en "\e[0m\n"
-echo ''
-
-} || echo "Welcome, $user!"
+echo "Welcome, $user!"
 
 # Prompt
 
